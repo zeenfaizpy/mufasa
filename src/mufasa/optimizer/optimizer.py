e8d39989ac59b5c1d08b0d1108ba4cb952145e2a
Mohammad Faizal <zeenfaiz@gmail.com>
2025-03-12 20:41:35 +0530

    added rule based optimizer - constant folding

--- src/mufasa/optimizer/optimizer.py
+++ src/mufasa/optimizer/optimizer.py
@@ -1,12 +1,46 @@
+from mufasa.logical_plan.operators import (
+    Scan, Projection, Filter, GroupBy
+)
+from mufasa.logical_plan.expressions import (
+    Column, Literal, Binary, Aggregate
+)
 
-def optimize_plan(plan):
-    pass
+
+class ConstantFolding:
+    def optimize(self, plan):
+        if isinstance(plan, Projection):
+            exprs = [self.fold_constants(expr) for expr in plan.expr]
+            return Projection(plan.child, exprs)
+        else:
+            return plan
+    
+    def fold_constants(self, expr):
+        if isinstance(expr, Binary):
+            left = self.fold_constants(expr.left)
+            right = self.fold_constants(expr.right)
+            if isinstance(left, Literal) and isinstance(right, Literal):
+                if expr.op == "+":
+                    return Literal(left.value + right.value)
+                elif expr.op == "-":
+                    return Literal(left.value - right.value)
+                elif expr.op == "*":
+                    return Literal(left.value * right.value)
+                elif expr.op == "/":
+                    return Literal(left.value / right.value)
+                else:
+                    raise Exception(f"Unsupported binary operation: {expr.op}")
+            return Binary(expr.name, left, expr.op, right)
+        else:
+            return expr
 
 
 class Optimizer:
-    def __init__(self, logical_plan):
-        self.logical_plan = logical_plan
+    def __init__(self):
+        self.rules = [
+            ConstantFolding()
+        ]
     
-    def optimize(self):
-        plan = optimize_plan(self.logical_plan)
+    def optimize(self, plan):
+        for rule in self.rules:
+            plan = rule.optimize(plan)
         return plan
